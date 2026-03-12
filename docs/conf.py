7e5f414be17e3f294aa763893fb82491b61d3bf4
Gael Varoquaux <gael.varoquaux@normalesup.org>
2026-03-12 10:12:10 +0100

    Simpler title

--- docs/conf.py
+++ docs/conf.py
@@ -59,4 +59,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'pydata_sphinx_theme'
+html_title = "TabICL" # A simpler title in the landing
+
 html_static_path = ['_static']
